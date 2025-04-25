2bddec9732481079395bc352b8ea58357dbd4ad2
Arthur Mariano <arthvm@proton.me>
2025-04-25 08:34:02 -0300

    feat: add header component and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,17 @@
+import { MenuIcon } from 'lucide-react'
+import Image from 'next/image'
+import { Button } from './ui/button'
+import { Card, CardContent } from './ui/card'
+
+export function Header() {
+  return (
+    <Card className="rounded-t-none">
+      <CardContent className="flex flex-row items-center justify-between px-5">
+        <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        <Button size={'icon'} variant={'outline'}>
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
+import { SearchIcon } from 'lucide-react'
+import Image from 'next/image'
+import { Header } from './_components/header'
 import { Button } from './_components/ui/button'
+import { Input } from './_components/ui/input'
 
 export default function Home() {
-  return <Button>Test</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5 space-y-6">
+        <div>
+          <h2 className="text-xl font-bold">Ola, Arthur!</h2>
+          <p>Sexta-feira, 25 de Abril</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Input placeholder="Faca sua busca..." />
+          <Button size={'icon'}>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative w-full h-[150px]">
+          <Image
+            src="/banner-01.png"
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="object-cover rounded-xl"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
