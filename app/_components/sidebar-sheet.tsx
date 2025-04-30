93d0d8cd7bab344461b34b48fdbc7ac8630e6955
Arthur Mariano <arthvm@proton.me>
2025-04-30 19:58:23 -0300

    chore(auth): add nextauth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,9 +1,17 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from 'lucide-react'
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { quickSearchOptions } from '../_constants/search'
 import { Avatar, AvatarImage } from './ui/avatar'
 import { Button } from './ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog'
 import { Separator } from './ui/separator'
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from './ui/sheet'
 
@@ -15,8 +23,37 @@ export function SiderbarSheet() {
       </SheetHeader>
 
       <div className="px-5 space-y-6 overflow-y-auto">
-        <div className="flex items-center gap-3">
-          <Avatar className="size-12 border-2 border-primary">
+        <div className="flex items-center gap-3 justify-between">
+          <h2 className="font-bold">Olá, Faça seu Login!</h2>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size={'icon'}>
+                <LogInIcon />
+              </Button>
+            </DialogTrigger>
+
+            <DialogContent className="w-[90%]">
+              <DialogHeader>
+                <DialogTitle>Faça login na plataforma</DialogTitle>
+                <DialogDescription>
+                  Conecte-se usando sua conta do Google
+                </DialogDescription>
+              </DialogHeader>
+
+              <Button variant={'outline'} className="gap-2 font-bold">
+                <Image
+                  src={'/google.svg'}
+                  alt="Logar com o google"
+                  width={18}
+                  height={18}
+                />
+                Google
+              </Button>
+            </DialogContent>
+          </Dialog>
+          {/*
+         <Avatar className="size-12 border-2 border-primary">
             <AvatarImage src={'https://github.com/arthvm.png'} />
           </Avatar>
 
@@ -24,6 +61,7 @@ export function SiderbarSheet() {
             <p className="font-bold">Arthur Mariano</p>
             <p className="text-xs">arthvm@proton.me</p>
           </div>
+          */}
         </div>
 
         <Separator />
